fe3dc5c7b6f5be35157d2ccc0293928c345a7881
Jack Bailey <Jack@Jacks-MacBook-Pro.local>
2016-10-13 16:34:27 +0100

    Commit!

--- iCandy/ViewController.swift
+++ iCandy/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var ipNameField: UITextField!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,20 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: Actions:
+    
+    @IBAction func sendButton(_ sender: UIButton) {
+        titleLabel.text = "Pngis"
+        Alamofire.request("https://httpbin.org/get").responseJSON{ response in
+                print(response.request)
+                print(response.response)
+                print(response.data)
+                print(response.result)
+            
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
 }
 
