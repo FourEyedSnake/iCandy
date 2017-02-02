aff360a9383d9e863b70685383cfa72d8dd297fe
Jack Bailey <Jack@Jacks-MacBook-Pro.local>
2017-02-02 16:13:42 +0000

    Made the button better

--- iCandy/ViewController.swift
+++ iCandy/ViewController.swift
@@ -35,6 +35,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*var leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipe"))
+        var rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipe"))
+        var upSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipe:"))
+        var downSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipe"))
+    
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        upSwipe.direction = .up
+        downSwipe.direction = .down
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)*/
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,43 +63,15 @@ class ViewController: UIViewController {
     // MARK: Actions:
     
     @IBAction func sendButton(_ sender: UIButton) {
-        //titleLabel.text = "Pngis"
-        
-        /*Alamofire.request("https://httpbin.org/get").responseJSON{ response in
-                print(response.request)
-                print(response.response)
-                print(response.data)
-                print(response.result)
-            
-                if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
-                }*/
         let jsonPara = self.createJSON()
-        /*Alamofire.request("http://192.168.1.46:2648/speak", method: .post, parameters: jsonPara, encoding: JSONEncoding.default).responseJSON(){ response in
-                //print(response.response)
-                print(response.debugDescription)
-        }*/
-        
-        //var alamoManager = Alamofire.SessionManager
-        
-       // alamoManager.default.session.configuration.timeoutIntervalForRequest = 0.1
-        
-        //alamoManager.default.session.configuration.timeoutIntervalForResource = 0.1
-        
-       /* Alamofire.SessionManager.default.session.configuration.timeoutIntervalForResource = 1
-        Alamofire.SessionManager.default.session.configuration.timeoutIntervalForRequest = 1*/
-        
-        //Alamofire.request("http://192.168.1.46:2648/speak", method: .post, parameters: jsonPara, encoding: JSONEncoding.default)
-        
-        //Alamofire.request(ipNameField.text!, method: .post, parameters: jsonPara, encoding: JSONEncoding.default)
-        
-        Alamofire.request("http://192.168.1.178:2648/speak", method: .post, parameters: jsonPara, encoding: JSONEncoding.default)
+        print(jsonPara)
+        let a:DataRequest
+        a = Alamofire.request("http://192.168.1.178:2648/speak", method: .post, parameters: jsonPara, encoding: JSONEncoding.default)
         
         print("we get here")
         
-        
-        
     }
+    
     @IBAction func updateLabel(_ sender: UISlider) {
         pitchLabel.text = "Pitch " + String(floor(pitchSlider.value * 100)) + "%"
     }
@@ -91,6 +80,21 @@ class ViewController: UIViewController {
     @IBAction func updateLabelSpeed(_ sender: AnyObject) {
         speedLabel.text = "Speed " + String(floor(speedSlider.value * 100)) + "%"
     }
+    
+/*    func handleSwipe(sender: UISwipeGestureRecognizer){
+        if(sender.direction == .left) {
+            print("LEFT")
+        }
+        if(sender.direction == .right) {
+            print("RIGHT")
+        }
+        if(sender.direction == .up) {
+            print("UP")
+        }
+        if(sender.direction == .down) {
+            print("DOWN")
+        }
+    }*/
 }
 
 //192.168.1.46:2648/speak
